38c7f783f0601ca9669a62fe420bc00b3994e9d4
ItStark <crmsnry@gmail.com>
2024-05-23 03:38:25 +0900

    Component updates and bug fixes

--- src/components/FreeSlotMatchComponent.vue
+++ src/components/FreeSlotMatchComponent.vue
@@ -8,7 +8,7 @@
     <div class="inputs">
       <div id="slotInputDiv">
         <label for="slots">Number of Slots:</label>
-        <input type="number" id="slots" min="1" max="5" v-model.number="numSlots" @input="generateGrid" />
+        <input type="number" id="slots" min="1" max="5" v-model.number="numSlots" @change="generateGrid" />
       </div>
 
       <div id="memberInputDiv">
@@ -29,6 +29,8 @@
           </div>
         </div>
       </div>
+
+      <button id="imitateBackendButton" title="Imitate Backend Data" @click="generateRandomArray">GET DATA</button>
     </div>
 
     <br>
@@ -85,7 +87,6 @@ export default {
     });
 
     const generateGrid = () => {
-      
        // Members Interval Check
       if (typeof state.numMembers != 'string' && state.numMembers < 1) 
         state.numMembers = 1;
@@ -97,7 +98,8 @@ export default {
         state.numSlots = 1;
       else if (typeof state.numSlots != 'string' && state.numSlots > 5) 
         state.numSlots = 5;
-      else { 
+      else {
+        // Rerender availability array by triggering reactive data 
         state.availabilityArray = Array.from({ length: state.numSlots }, () => 
           Array.from({ length: state.numMembers }, () => 0)
         );
@@ -122,16 +124,26 @@ export default {
       state.availabilityArray[rowIndex][colIndex] = nextState;
     };
 
+    const generateRandomArray = () => {
+      state.availabilityArray = Array.from({ length: state.numSlots }, () =>
+        Array.from({ length: state.numMembers }, () => Math.floor(Math.random() * 3))
+      );
+    }
+
     const gridStyle = computed(() => {
-      console.warn(window.innerWidth)
+      // TODO: Better width handling
+      const gridSize = window.innerWidth > 768 ? 50 : 30;
+
       return {
-        gridTemplateColumns: `repeat(${state.numMembers}, ` +  window.innerWidth > 768 ? ` 50px)` : ` 30px)`
+        gridTemplateColumns: `repeat(${state.numMembers}, ${gridSize}px`
       };
     });
 
      const reactiveRowStyle = computed(() => {
+      const gridSize = window.innerWidth > 768 ? 40 : 25;
+
       return {
-        gridTemplateColumns: `repeat(${state.numMembers}, 1fr`
+        gridTemplateColumns: `repeat(${state.numMembers}, ${gridSize}px`
       };
     });
 
@@ -140,11 +152,13 @@ export default {
     watch(() => state.numMembers, generateGrid);
 
     generateGrid(); // Initialize the grid with default values
+    generateRandomArray() // Imitate Receiving Backend Data initially
 
     return {
       ...toRefs(state),
       getColorClass,
       toggleState,
+      generateRandomArray,
       generateGrid,
       gridStyle,
       reactiveRowStyle
@@ -153,7 +167,6 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 h3 {
@@ -192,7 +205,6 @@ label {
 
 .grid {
   display: grid;
-  grid-template-columns: repeat(5, 50px);
   gap: 10px;
   place-content: center;
   margin: 30px auto 0px;
@@ -221,8 +233,14 @@ label {
   background-color:  #ad2c19;
 }
 
-#statesDiv {
-  
+#imitateBackendButton {
+    margin: 10px;
+    border-radius: 6px;
+    border: 1px solid #000;
+    padding: 4px 8px;
+    background: #000;
+    color: #fff;
+    cursor: pointer;
 }
 
 .stateColor {
@@ -277,7 +295,67 @@ label {
 }
 
 /* RESPONSIVENESS */
-@media (max-width: 768px)  {
+/* Extra small devices (phones, less than 576px) */
+@media (max-width: 575.98px) {
+
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(5, 30px);
+    gap: 6px;
+    margin: 8px auto 0px;
+    width: fit-content;
+    padding: 18px;
+    border-radius: 6px;
+  }
+  .row {
+    display: contents;
+  }
+  .cell {
+    width: 30px;
+    height: 30px;
+    border: 1px solid #000;
+    border-radius: 6px;
+    cursor: pointer;
+  }
+
+  .inputs {
+    display: grid;
+    font-size: 16px;
+  }
+
+  #slotInputDiv {
+    margin-bottom: 16px;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  #slotInputDiv > input {
+    font-size: 16px !important;
+  }
+
+  #memberInputsDiv {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  #memberInputsDiv > input {
+    font-size: 16px !important;
+  }
+
+  #footer {
+    font-size: 12px;
+    display: grid;
+  }
+
+  #grayBox, #greenBox, #redBox {
+    width: 30px;
+    height: 30px;
+  }
+}
+
+
+/* Small devices (tablets, 576px and up) */
+@media (min-width: 576px) and (max-width: 767.98px) {
   .mainContainer {
     border: 2px solid #5E5E5E;
     border-radius: 8px;
@@ -339,7 +417,9 @@ label {
   }
 }
 
-@media (min-width: 768px) {
+
+/* Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) and (max-width: 991.98px) {
   .mainContainer {
     border: 2px solid #5E5E5E;
     border-radius: 8px;
@@ -382,5 +462,57 @@ label {
     display: flex;
     justify-content: space-between;
   }
+}
+
+
+/* Large devices (desktops, 992px and up) */
+@media (min-width: 992px) and (max-width: 1199.98px) {
+  .mainContainer {
+    border: 2px solid #5E5E5E;
+    border-radius: 8px;
+    padding: 40px;
+  }
+
+  .grid {
+    display: grid;
+    gap: 10px;
+    place-content: center;
+    margin: 30px auto 0px;
+    background-color: #000;
+    width: fit-content;
+    padding: 32px;
+    border-radius: 8px;
+  }
+  .row {
+    display: contents;
+  }
+  .cell {
+    width: 50px;
+    height: 50px;
+    border: 1px solid #000;
+    border-radius: 6px;
+    cursor: pointer;
+  }
+
+  .inputs {
+    display: flex;
+  }
+
+  #slotInputsDiv {
+    margin-bottom: 6px;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  #memberInputsDiv {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+
+
+/* Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {
+
 }
 </style>
